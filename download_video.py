94eb0f967eba80fd4c6ef30e289e886eb74e9b97
SeniorSoftwareEngineer01 <thekingdomof01@gmail.com>
2024-12-31 12:55:54 +0100

    create fill for test

---
+++ download_video.py
@@ -0,0 +1,23 @@
+import yt_dlp
+
+url = 'https://www.youtube.com/watch?v=zmjbQzZ9oWE'
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Referer': 'https://www.youtube.com/',
+    'DNT': '1',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'TE': 'Trailers',
+}
+
+ydl_opts = {
+    'headers': headers,
+    'format': 'best',
+    'outtmpl': '%(title)s.%(ext)s',
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
